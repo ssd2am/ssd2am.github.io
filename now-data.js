0c3e4885c943e1726be9e46aa03b830ce9b343b1
ssd2am <ranacid1569@gmail.com>
2026-02-27 00:56:22 +0900

    midnight tinkering

--- now-data.js
+++ now-data.js
@@ -3,7 +3,7 @@
 const nowTweets = [
 
 
-
+    {content: "곧 봄이 올것 같아서 멈춰두었던 <a href='https://www.banul.co.kr/shop/shopdetail.html?branduid=10701037&xcode=152&mcode=001&scode=&type=Y&sort=regdate&cur_code=152&search=&GfDT=amh3U1U%3D'>로우키 가디건</a> 뜨개를 마무리하려구 꺼냄! 주말에 오랜만에 카뜨라도 해볼까 싶어🫖"},
     {content: "대표쨩이 아침부터 과자파티 열어줘서 간식으로 점심 떼움. 여기 혹시 유치원인가요?<br>🎼오노추 - <a href='https://youtu.be/ALOSFSILnpk'>LET ME KNOW - 100円キッス</a>"},
     {content: "hi! i'm testing my now page with Brixton!"},
     {content: "This is an older update. It will show up in the archive but not in the sidebar!"}
