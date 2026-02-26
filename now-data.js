4f982d52680f85cf4303164216b32cbc59e4f208
ssd2am <ranacid1569@gmail.com>
2026-02-26 15:16:14 +0900

    Update now-data.js

--- now-data.js
+++ now-data.js
@@ -5,7 +5,7 @@ const nowTweets = [
 
 
 
-    {content: "hi! i'm testing my now page"},
+    {content: "hi! i'm testing my now page with Brixton!"},
     {content: "대표쨩이 아침부터 과자파티 열어줘서 간식으로 점심 떼움. 여기 혹시 유치원인가요?<br>🎼오노추 - <a href='https://youtu.be/ALOSFSILnpk'>LET ME KNOW - 100円キッス</a>"},
     {content: "This is an older update. It will show up in the archive but not in the sidebar!"}
 
