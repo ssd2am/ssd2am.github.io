692dac9b6727f67835aceddbf8f9f15b1145c770
ssd2am <ranacid1569@gmail.com>
2026-03-09 02:06:06 +0900

    big update

--- now-data.js
+++ now-data.js
@@ -2,11 +2,11 @@
 
 const nowTweets = [
 
-
-    {content: "곧 봄이 올것 같아서 멈춰두었던 <a href='https://www.banul.co.kr/shop/shopdetail.html?branduid=10701037&xcode=152&mcode=001&scode=&type=Y&sort=regdate&cur_code=152&search=&GfDT=amh3U1U%3D'>로우키 가디건</a> 뜨개를 마무리하려구 꺼냄! 주말에 오랜만에 카뜨라도 해볼까 싶어🫖"},
-    {content: "대표쨩이 아침부터 과자파티 열어줘서 간식으로 점심 떼움. 여기 혹시 유치원인가요?<br>🎼오노추 - <a href='https://youtu.be/ALOSFSILnpk'>LET ME KNOW - 100円キッス</a>"},
-    {content: "hi! i'm testing my now page with Brixton!"},
-    {content: "This is an older update. It will show up in the archive but not in the sidebar!"}
+    {content: "사람이 만나고 싶어지고 수다가 그리운걸 보니 다시 봄인가 봄🍀 주말에 AI없이 코딩하기라는 셀프 챌린지를 내렸고 달팽이 페이스로 조금씩 페이지를 손 봄..."},
+    {content: "갑작스러운 업무 러시에 잠시 웹사이트 개발 쉬어감🫠 주말에 칭긔들이랑 로망의 코딩팟 갖기루 함!! 너무 신나!!"},
+    {content: "곧 봄이 올것 같아서 멈춰두었던 <a href='https://www.banul.co.kr/shop/shopdetail.html?branduid=10701037&xcode=152&mcode=001&scode=&type=Y&sort=regdate&cur_code=152&search=&GfDT=amh3U1U%3D' target='blank'>로우키 가디건</a> 뜨개를 마무리하려구 꺼냄! 주말에 오랜만에 카뜨라도 해볼까 싶어🫖"},
+    {content: "대표쨩이 아침부터 과자파티 열어줘서 간식으로 점심 떼움. 여기 혹시 유치원인가요?<br>🎼오노추 - <a href='https://youtu.be/ALOSFSILnpk' target='blank'>LET ME KNOW - 100円キッス</a>"},
+    {content: "hi! i'm testing my now page with Brixton!"}
 
 
 
