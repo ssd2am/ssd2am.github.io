9ce1c31f8ce9680bf337ef60bbe0308e23965923
ssd2am <ranacid1569@gmail.com>
2026-03-11 13:39:04 +0900

    fix typo

--- now-data.js
+++ now-data.js
@@ -1,7 +1,7 @@
 // {content: ""},
 
 const nowTweets = [
-    {content: "<a href='https://www.shoemaru.com/' target='_blank'>슈마루 폐업<a> 소식 지금 들음😭 메일보고 헐레벌떡 달려갔는데 이미 재고 다 털렸네ㅠ 사실 슈마루 마약샌들 안 산지 몇 년 됐는데 폐업한다 하니 또 슬프고 아쉬운게 사람 심리인가 봐... 있을 때 잘하자"},
+    {content: "<a href='https://www.shoemaru.com/' target='_blank'>슈마루 폐업</a> 소식 지금 들음😭 메일보고 헐레벌떡 달려갔는데 이미 재고 다 털렸네ㅠ 사실 슈마루 마약샌들 안 산지 몇 년 됐는데 폐업한다 하니 또 슬프고 아쉬운게 사람 심리인가 봐... 있을 때 잘하자"},
     {content: "사람이 만나고 싶어지고 수다가 그리운걸 보니 다시 봄인가 봄🍀 주말에 AI없이 코딩하기라는 셀프 챌린지를 내렸고 달팽이 페이스로 조금씩 페이지를 손 봄..."},
     {content: "갑작스러운 업무 러시에 잠시 웹사이트 개발 쉬어감🫠 주말에 칭긔들이랑 로망의 코딩팟 갖기루 함!! 너무 신나!!"},
     {content: "곧 봄이 올것 같아서 멈춰두었던 <a href='https://www.banul.co.kr/shop/shopdetail.html?branduid=10701037&xcode=152&mcode=001&scode=&type=Y&sort=regdate&cur_code=152&search=&GfDT=amh3U1U%3D' target='blank'>로우키 가디건</a> 뜨개를 마무리하려구 꺼냄! 주말에 오랜만에 카뜨라도 해볼까 싶어🫖"},
